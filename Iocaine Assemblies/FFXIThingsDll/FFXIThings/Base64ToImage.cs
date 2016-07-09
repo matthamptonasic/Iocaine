533d5d7eb364041efb4538d8713a3a7e99eef440
matthamptonasic <matthamptonasic@gmail.com>
2016-07-08 20:49:45 -0700

    FFXI Things Initial
    Initial port from CVS.

---
+++ Iocaine Assemblies/FFXIThingsDll/FFXIThings/Base64ToImage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Iocaine2.Data.Client
+{
+    class Base64ToImage
+    {
+        public static Image ConvertThis(string ImageText)
+        {
+            if (ImageText.Length > 0)
+            {
+                Byte[] bitmapData = new Byte[ImageText.Length];
+                bitmapData = Convert.FromBase64String(FixBase64ForImage(ImageText));
+
+                System.IO.MemoryStream streamBitmap = new System.IO.MemoryStream(bitmapData);
+
+                Bitmap bitImage = new Bitmap((Bitmap)Image.FromStream(streamBitmap));
+                return bitImage;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private static string FixBase64ForImage(string Image)
+        {
+            System.Text.StringBuilder sbText = new System.Text.StringBuilder(Image, Image.Length);
+
+            sbText.Replace("\r\n", String.Empty);
+
+            sbText.Replace(" ", String.Empty);
+
+            return sbText.ToString();
+        }
+    }
+}
---
+++ Iocaine Assemblies/FFXIThingsDll/FFXIThings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FFXIThings")]
+[assembly: AssemblyDescription("Exports item details for Final Fantasy XI. Initially written for Iocaine2 multi-functional application, but may be used in other applications with the consent of hammy025 @ www.ffxiscripting.com.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Iocaine2")]
+[assembly: AssemblyProduct("FFXIThings")]
+[assembly: AssemblyCopyright("Copyright © Iocaine2 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c19b1b3c-2b1b-4fe0-b210-e8e5845c91c0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
