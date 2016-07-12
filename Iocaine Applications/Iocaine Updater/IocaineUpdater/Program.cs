67179df9457d02238f121e1a9a78074ac4e4926d
matthamptonasic <matthamptonasic@gmail.com>
2016-07-11 21:37:30 -0700

    Import from CVS.
    Also removed references to the Updater class directly in the code.
    Now those calls are to the referenced assembly IocaineServer.dll.
    Also uses the IocaineCore.dll assembly instead of individual files due
    to
    interdependencies.

---
+++ Iocaine Applications/Iocaine Updater/IocaineUpdater/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+using Iocaine2;
+using Iocaine2.Logging;
+
+namespace IocaineUpdater
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            const bool PromptForUpdates = false;
+            const String mainFile = ".\\Main.exe";
+
+            //Set up log file.
+            LoggingFunctions.SetLogFile("Updater Debug Log.txt");
+            LoggingFunctions.Timestamp("Beginning Iocaine Update.");
+
+            Server.Update.Init_Iocaine();
+
+            try
+            {
+                DisplayBox updateDisplay = new DisplayBox(PromptForUpdates);
+                updateDisplay.ShowDialog();
+            }
+            catch (Exception Ex)
+            {
+                LoggingFunctions.Error("While updating files using display box: " + Ex);
+            }
+
+            String mainFileAbsolute = Path.GetFullPath(mainFile);
+            if (File.Exists(mainFileAbsolute))
+            {
+                try
+                {
+                    Process IocaineMain = new Process();
+                    IocaineMain.StartInfo.FileName = mainFileAbsolute;
+                    IocaineMain.Start();
+                }
+                catch (Exception Ex)
+                {
+                    LoggingFunctions.Error("Starting main process: " + Ex);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Could not find " + mainFileAbsolute + "!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
