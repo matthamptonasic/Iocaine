74cb2f18f62eca2edb2c13c4e3b671b6512cbd34
matthamptonasic <matthamptonasic@gmail.com>
2016-07-21 00:05:31 -0700

    Split out from Actions.cs

---
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Actions/Conditions/Condition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Iocaine2.Data.Client;
+using Iocaine2.Logging;
+
+namespace Iocaine2.Data.Structures
+{
+    public abstract partial class Action
+    {
+        public abstract class Condition
+        {
+            #region Enums
+            public enum CONDITION_TYPE : uint
+            {
+                JOB_LVL,
+                INV_ITEM,
+                KEY_ITEM
+            }
+            #endregion Enums
+
+            #region Private Members
+            protected CONDITION_TYPE m_conditionType;
+            #endregion Private Members
+
+            #region Public Properties
+            #endregion Public Properties
+
+            public Condition(CONDITION_TYPE iType)
+            {
+                m_conditionType = iType;
+            }
+
+            #region Public Methods
+            public abstract bool IsSatisfied();
+            #endregion Public Methods
+        }
+    }
+}
