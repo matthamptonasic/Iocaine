9cd219ea3429eac62441760b31ccacbc1c2a72e2
matthamptonasic <matthamptonasic@gmail.com>
2016-08-06 04:00:18 -0700

    Initial

---
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Actions/Conditions/ConditionAnd.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Iocaine2.Data.Client;
+using Iocaine2.Logging;
+
+namespace Iocaine2.Data.Structures
+{
+    public abstract partial class Action
+    {
+        public class ConditionAnd : Condition
+        {
+            #region Enums
+            #endregion Enums
+
+            #region Private Members
+            private Condition m_condition0;
+            private Condition m_condition1;
+            #endregion Private Members
+
+            #region Public Properties
+            #endregion Public Properties
+
+            public ConditionAnd(Condition iCond0, Condition iCond1, string iName, bool iInvert = false)
+                : base(CONDITION_TYPE.AND, iName, iInvert)
+            {
+                m_condition0 = iCond0;
+                m_condition1 = iCond1;
+            }
+
+            #region Public Methods
+            public override bool Evaluate()
+            {
+                if ((m_condition0 == null) || (m_condition1 == null))
+                {
+                    return false;
+                }
+                bool retVal = m_condition0.Evaluate() && m_condition1.Evaluate();
+                return Not ? !retVal : retVal;
+            }
+            #endregion Public Methods
+        }
+    }
+}
---
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Actions/Conditions/ConditionOr.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Iocaine2.Data.Client;
+using Iocaine2.Logging;
+
+namespace Iocaine2.Data.Structures
+{
+    public abstract partial class Action
+    {
+        public class ConditionOr : Condition
+        {
+            #region Enums
+            #endregion Enums
+
+            #region Private Members
+            private Condition m_condition0;
+            private Condition m_condition1;
+            #endregion Private Members
+
+            #region Public Properties
+            #endregion Public Properties
+
+            public ConditionOr(Condition iCond0, Condition iCond1, string iName, bool iInvert = false)
+                : base(CONDITION_TYPE.OR, iName, iInvert)
+            {
+                m_condition0 = iCond0;
+                m_condition1 = iCond1;
+            }
+
+            #region Public Methods
+            public override bool Evaluate()
+            {
+                bool retVal = false;
+                if ((m_condition0 == null) && (m_condition1 == null))
+                {
+                    return false;
+                }
+                else if ((m_condition0 == null) || (m_condition1 == null))
+                {
+                    if (m_condition0 == null)
+                    {
+                        retVal = m_condition1.Evaluate();
+                    }
+                    else
+                    {
+                        retVal = m_condition0.Evaluate();
+                    }
+                }
+                else
+                {
+                    retVal = m_condition0.Evaluate() || m_condition1.Evaluate();
+                }
+                return Not ? !retVal : retVal;
+            }
+            #endregion Public Methods
+        }
+    }
+}
