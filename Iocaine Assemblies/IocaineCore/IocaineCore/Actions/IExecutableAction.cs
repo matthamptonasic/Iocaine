7db937d32bf95701300e67130907cce654f5b85f
matthamptonasic <matthamptonasic@gmail.com>
2016-08-06 20:26:37 -0700

    Initial.

---
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Actions/IExecutableAction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Iocaine2.Data.Structures
+{
+    public interface IExecutableAction
+    {
+        bool IsCapable();
+        bool CanPerform();
+        bool Execute(string iTarget = "");
+    }
+}
