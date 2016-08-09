591ab57ac2d451d9bc6b7c4852e71263191c2788
matthamptonasic <matthamptonasic@gmail.com>
2016-08-09 04:48:28 -0700

    Added IsBlocking property.
    This is required to be executed.

--- Iocaine Assemblies/IocaineCore/IocaineCore/Actions/IExecutableAction.cs
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Actions/IExecutableAction.cs
@@ -4,6 +4,7 @@ namespace Iocaine2.Data.Structures
 {
     public interface IExecutableAction
     {
+        bool IsBlocking { get; }
         bool IsCapable();
         bool CanPerform();
         bool Execute(string iTarget = "");
