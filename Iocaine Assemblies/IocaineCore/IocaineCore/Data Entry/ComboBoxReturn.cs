08063384833c367b16ac7d973aad28181d302b99
matthamptonasic <matthamptonasic@gmail.com>
2016-08-11 06:02:12 -0700

    Added data entry form return value classes.

---
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Data Entry/ComboBoxReturn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iocaine2.Data.Entry
+{
+    public class ComboBoxReturn<T> : ControlReturn
+    {
+        #region Private Members
+        private T m_value;
+        #endregion Private Members
+
+        #region Public Properties
+        public T Value
+        {
+            get
+            {
+                return m_value;
+            }
+        }
+        #endregion Public Properties
+
+        #region Constructor
+        public ComboBoxReturn(T iValue)
+        {
+            m_value = iValue;
+        }
+        #endregion Constructor
+    }
+}
---
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Data Entry/UpDownReturn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iocaine2.Data.Entry
+{
+    public class UpDownReturn : ControlReturn
+    {
+        #region Private Members
+        private decimal m_value;
+        #endregion Private Members
+
+        #region Public Properties
+        public decimal Value
+        {
+            get
+            {
+                return m_value;
+            }
+        }
+        #endregion Public Properties
+
+        #region Constructor
+        public UpDownReturn(decimal iValue)
+        {
+            m_value = iValue;
+        }
+        #endregion Constructor
+    }
+}
