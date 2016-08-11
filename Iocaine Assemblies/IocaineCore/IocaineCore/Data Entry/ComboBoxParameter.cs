540ccf9af00f2099fb756e1f84dbc07a9186ed76
matthamptonasic <matthamptonasic@gmail.com>
2016-08-11 06:01:41 -0700

    Initial.

---
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Data Entry/ComboBoxParameter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iocaine2.Data.Entry
+{
+    public class ComboBoxParameter<T> : ControlParameter
+    {
+        #region Private Members
+        private List<T> m_items;
+        #endregion Private Members
+
+        #region Public Properties
+        public List<T> Items
+        {
+            get
+            {
+                return m_items;
+            }
+        }
+        #endregion Public Properties
+
+        #region Constructor
+        public ComboBoxParameter(string iLabelText, List<T> iItems)
+            : base(iLabelText)
+        {
+            m_items = iItems;
+        }
+        #endregion Constructor
+
+        #region Public Methods
+        #endregion Public Methods
+
+        #region Private Methods
+        #endregion Private Methods
+    }
+}
