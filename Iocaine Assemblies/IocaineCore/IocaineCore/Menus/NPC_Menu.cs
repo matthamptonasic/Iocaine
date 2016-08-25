f07f6a8cf3d43e64d2661e50a7861688bc64bd43
matthamptonasic <matthamptonasic@gmail.com>
2016-08-25 04:08:21 -0700

    WIP: As we load the first menu to parse.

--- Iocaine Assemblies/IocaineCore/IocaineCore/Menus/NPC_Menu.cs
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Menus/NPC_Menu.cs
@@ -12,21 +12,77 @@ namespace Iocaine2.Data.Client
 
         #region Private Members
         private string m_topText;
+        private bool m_isTop;
         private NPCs.NPC_TYPE m_npcType;
+        private List<NPCs.NPC_INFO> m_npcInfo;
+        private bool m_npcsAreUnique = true;
+        private List<NPC_MenuNode> m_nodes;
+        private NPC_MenuParsingParameters m_parsingParams;
         #endregion Private Members
 
         #region Public Properties
+        public bool NpcsAreUnique
+        {
+            get
+            {
+                return m_npcsAreUnique;
+            }
+            set
+            {
+                m_npcsAreUnique = value;
+            }
+        }
+        public NPC_MenuParsingParameters ParsingParams
+        {
+            get
+            {
+                return m_parsingParams;
+            }
+            set
+            {
+                m_parsingParams = value;
+            }
+        }
         #endregion Public Properties
 
         #region Constructors
-        public NPC_Menu(NPCs.NPC_TYPE iNpcType, string iTopText)
+        public NPC_Menu(NPCs.NPC_TYPE iNpcType, string iTopText, bool iIsTopMenu = false)
         {
             m_npcType = iNpcType;
             m_topText = iTopText;
+            m_isTop = iIsTopMenu;
         }
         #endregion Constructors
 
         #region Public Methods
+        public void Add(NPC_MenuNode iNode)
+        {
+            if (iNode != null)
+            {
+                m_nodes.Add(iNode);
+            }
+        }
+        public NPC_MenuNode GetNode(string iText, bool iMatchPartial = true)
+        {
+            foreach (NPC_MenuNode node in m_nodes)
+            {
+                if (iMatchPartial)
+                {
+                    if (node.Text.Contains(iText))
+                    {
+                        return node;
+                    }
+                }
+                else
+                {
+                    if (node.Text == iText)
+                    {
+                        return node;
+                    }
+                }
+            }
+            return null;
+        }
         #endregion Public Methods
 
         #region Private Methods
