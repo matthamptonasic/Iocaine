d81eaa2083059ea7cbf910a528d236ef10d69c0c
matthamptonasic <matthamptonasic@gmail.com>
2016-08-24 20:41:26 -0700

    Initial placeholder.

---
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Menus/NPC_MenuNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iocaine2.Data.Client
+{
+    public class NPC_MenuNode
+    {
+        #region Enums
+        #endregion Enums
+
+        #region Private Members
+        private string m_text;
+        private bool m_finalAction = false;
+        private bool m_confirmationPositive = false;
+        private bool m_confirmationNegative = false;
+        private bool m_pagedListItem = false;
+        private bool m_exitAll = false;
+        private bool m_movesUp = false;
+        private bool m_movesDown = false;
+        private bool m_movesLeft = false;
+        private bool m_movesRight = false;
+        private bool m_jumps = false;
+        private NPC_Menu m_link;
+        #endregion Private Members
+
+        #region Public Properties
+        #endregion Public Properties
+
+        #region Constructor
+        #endregion Constructor
+
+        #region Public Methods
+        #endregion Public Methods
+
+        #region Private Methods
+        #endregion Private Methods
+    }
+}
