003e175c02b93bbd5e64f7fa227971f9f8fa6ab3
matthamptonasic <matthamptonasic@gmail.com>
2016-08-24 20:41:15 -0700

    Initial placeholder.

---
+++ Iocaine Assemblies/IocaineCore/IocaineCore/Menus/NPC_Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iocaine2.Data.Client
+{
+    public class NPC_Menu
+    {
+        #region Enums
+        #endregion Enums
+
+        #region Private Members
+        #endregion Private Members
+
+        #region Public Properties
+        #endregion Public Properties
+
+        #region Constructor
+        #endregion Constructor
+
+        #region Public Methods
+        #endregion Public Methods
+
+        #region Private Methods
+        #endregion Private Methods
+    }
+}
