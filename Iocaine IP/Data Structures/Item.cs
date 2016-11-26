e0bf47a2a3ef9aad02e8369a5ebcca2157723dcc
matthamptonasic <matthamptonasic@gmail.com>
2016-11-26 00:00:36 -0800

    Moved ITEM_TYPE enum to Things DLL.
    Also reorganized the file for consistancy.

--- Iocaine IP/Data Structures/Item.cs
+++ Iocaine IP/Data Structures/Item.cs
@@ -3,56 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Iocaine2.Data.Client;
+
 namespace Iocaine2.Inventory
 {
     public class Item
     {
-        #region Constructors
-        public Item(String iName, ushort iID, ITEM_TYPE iType)
-        {
-            name = iName;
-            itemID = iID;
-            type = iType;
-        }
-        public Item(Item iItem)
-        {
-            name = iItem.Name;
-            itemID = iItem.ItemID;
-            type = iItem.Type;
-        }
-        #endregion Constructors
         #region Enums
-        public enum ITEM_TYPE : byte
-        {
-            MISC_TEMP_ITEMS = 0x1,  //Mostly things like fanatics drink, or dusty items, but also distilled water
-            MISC_TESTIMONY = 0x2,   //Also various orbs
-            FISH = 0x3,
-            WEAPON = 0x4,
-            ARMOR = 0x5,
-            LINKPEARL = 0x6,
-            MISC_KEY = 0x7, //Also sickle, hatchet, orbs, cards, ammo pouches, some fish
-            CRYSTALS = 0x8,
-            FURNISHING = 0xA,
-            SEEDS = 0xB,
-            FLOWER_POT = 0xC,
-            MANNEQUIN = 0xE,
-            CODEX = 0xF,    //Various book pages
-            CHOCO_ITEMS_A = 0x10,
-            CHOCO_ITEMS_B = 0x11,
-            SOUL_PLATE = 0x12,
-            SOUL_REFLECTOR = 0x13,
-            ASSAULT_ITEMS = 0x14,
-            MOG_MARBLE = 0x15,
-            UNKNOWN = 0xFF
-        }
         #endregion Enums
-        #region Member Variables
+
         #region Private Members
         private String name;
         private ushort itemID;
-        private ITEM_TYPE type;
+        private Things.ITEM_TYPE type;
         #endregion Private Members
-        #region Public Members
+
+        #region Public Properties
         public String Name
         {
             get
@@ -75,7 +41,7 @@ namespace Iocaine2.Inventory
                 itemID = value;
             }
         }
-        public ITEM_TYPE Type
+        public Things.ITEM_TYPE Type
         {
             get
             {
@@ -86,8 +52,23 @@ namespace Iocaine2.Inventory
                 type = value;
             }
         }
-        #endregion Public Members
-        #endregion Member Variables
+        #endregion Public Properties
+
+        #region Constructors
+        public Item(String iName, ushort iID, Things.ITEM_TYPE iType)
+        {
+            name = iName;
+            itemID = iID;
+            type = iType;
+        }
+        public Item(Item iItem)
+        {
+            name = iItem.Name;
+            itemID = iItem.ItemID;
+            type = iItem.Type;
+        }
+        #endregion Constructors
+        
         #region Methods
         #endregion Methods
     }
