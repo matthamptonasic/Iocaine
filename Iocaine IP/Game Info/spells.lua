1742a4d65025c823457b64f78eb0d388a5aaf982
matthamptonasic <matthamptonasic@gmail.com>
2017-04-22 22:49:34 -0700

    Copyright update only.

--- Iocaine IP/Game Info/spells.lua
+++ Iocaine IP/Game Info/spells.lua
@@ -957,7 +957,7 @@ return {
 }, {"id", "en", "ja", "cast_time", "element", "icon_id", "icon_id_nq", "levels", "mp_cost", "prefix", "range", "recast", "recast_id", "requirements", "skill", "targets", "type", "duration"}
 
 --[[
-Copyright © 2013-2016, Windower
+Copyright © 2013-2017, Windower
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
