2257af4f3c405b09ae2186929de27090bf0ef9be
matthamptonasic <matthamptonasic@gmail.com>
2017-07-25 00:36:39 -0700

    Initial socket server.
    Skeleton.

---
+++ Iocaine Assemblies/IocaineCore/IocaineCore/SocketServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Iocaine2
+{
+    public static class SocketServer
+    {
+        #region Private Members
+        #endregion Private Members
+        #region Public Properties
+        #endregion Public Properties
+        #region Public Methods
+        public static void Start()
+        {
+
+        }
+        #endregion Public Methods
+        #region Private Methods
+        #endregion Private Methods
+    }
+}
